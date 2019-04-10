3b12ad7ab1a8e332d210686157e1538a12e2460c
bdebowski <bazyli.d@gmail.com>
2019-04-09 20:36:27 -0400

    Added PID Control

--- src/env/environment.py
+++ src/env/environment.py
@@ -2,6 +2,7 @@ from src.common.iupdatable import IUpdatable
 from src.env.cart import Cart
 from src.gfx.rectgfx import RectGfx
 from src.env.override_controller import OverrideController
+from src.env.pid_controller import PIDController
 
 
 class Environment(IUpdatable):
@@ -16,8 +17,11 @@ class Environment(IUpdatable):
         self._followcart = Cart(self._follow_centre)
         self._updatables.append(self._followcart)
 
-        self._override_controller = OverrideController(self._leadcart, self._lead_centre, 100.0, 3.0)
+        self._override_controller = OverrideController(self._leadcart, self._lead_centre, 150.0, 5.0)
         self._updatables.append(self._override_controller)
+
+        self._pid_controller = PIDController(self._followcart, self._leadcart, 0.0)
+        self._pid_controller.run()
 
     def update(self, dt_sec):
         # update self
---
+++ src/env/pid_controller.py
@@ -0,0 +1,38 @@
+import time
+from multiprocessing import Process
+
+
+class PIDController:
+    def __init__(self, cart_to_control, cart_to_follow, target_distance=0.0):
+        self._cart_control = cart_to_control
+        self._cart_follow = cart_to_follow
+        self._dist_tgt = target_distance
+
+    def run(self):
+        p = Process(target=self._run, daemon=True)
+        p.start()
+
+    def _run(self):
+        p = 25
+        i = 0.015
+        d = 1.0
+        decay = 0.99
+
+        err_accum = 0.0
+        err_prev = 0.0
+        t_prev = time.time()
+        while True:
+            time.sleep(0.001)
+            t_now = time.time()
+            dt_sec = t_now - t_prev
+            t_prev = t_now
+
+            d_obs = self._cart_control.position()[0] - self._cart_follow.position()[0]
+
+            err = d_obs - self._dist_tgt
+            d_err = (err - err_prev) / dt_sec
+            err_accum = decay * err_accum + err
+            err_prev = err
+
+            acc_applied = p * -err + i * -err_accum + d * -d_err
+            self._cart_control.acceleration = (acc_applied, 0.0)
