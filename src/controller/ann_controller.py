317f6533204ad1f027f56f24822ec559f800c31f
bdebowski <bazyli.d@gmail.com>
2019-04-18 03:36:35 -0400

    Moved value network to its own class.  Added batching

--- src/controller/ann_controller.py
+++ src/controller/ann_controller.py
@@ -1,3 +1,4 @@
+import os
 import time
 import math
 from multiprocessing import Process
@@ -23,7 +24,7 @@ class ANNController:
         self._hist_sz = 150
         self._xerr_hist_lscale = RotatingBuffer(self._hist_sz)  # normalized by cart length
         self._dt_sec_hist = RotatingBuffer(self._hist_sz)
-        self._tau = 1.0
+        self._tau = 0.25
         # These values are in units of environment width/height.
         # top-right corner: xpos = 0.5, ypos = 0.5
         # bottom-left corner: xpos = -0.5, ypos = -0.5
@@ -50,15 +51,8 @@ class ANNController:
             np.array([0.0], np.float32)])
 
         # Create the Value Estimation Network
-        self._vin = np.zeros((1, 11), np.float32)
-        self._v = tf.keras.Sequential()
-        self._v.add(tf.keras.layers.Dense(11, activation="sigmoid", input_dim=11))
-        self._v.add(tf.keras.layers.Dense(7, activation="sigmoid"))
-        self._v.add(tf.keras.layers.Dense(3, activation="sigmoid"))
-        self._v.add(tf.keras.layers.Dense(1, activation=None))
-
-        self._base_lr = 0.0001
-        self._optimizer = tf.optimizers.SGD(self._base_lr)
+        self._vin = np.zeros(11, np.float32)
+        self._vnet = _ValueNetwork(r"D:\Users\Bazyli\Dropbox\PycharmProjects\lead-follow\model\checkpoints")
 
     def _get_future_value_at_oldest_hist(self):
         """
@@ -98,8 +92,8 @@ class ANNController:
         self._relative_cart_xvel_hist_lscale.set_next(self._relative_cart_obs_state_lscale.vel[0])
         self._relative_cart_xacc_hist_lscale.set_next(self._relative_cart_obs_state_lscale.acc[0])
 
-    def _set_vin(self):
-        self._vin[0] = [
+    def _get_vin(self):
+        self._vin = [
             self._followed_cart_xpos_hist_gscale.get_prev(self._hist_sz - 1),
             self._followed_cart_xvel_hist_gscale.get_prev(self._hist_sz - 1),
             self._followed_cart_xacc_hist_gscale.get_prev(self._hist_sz - 1),
@@ -111,6 +105,7 @@ class ANNController:
             self._relative_cart_xacc_hist_lscale.get_prev(self._hist_sz - 1),
             self._xaction_hist_lscale.get_prev(self._hist_sz - 1),
             self._xerr_hist_lscale.get_prev(self._hist_sz - 1)]
+        return self._vin
 
     @staticmethod
     def run(cart_to_control, cart_to_follow, xspan, yspan, target_distance_cart_lengths=0.0):
@@ -131,11 +126,7 @@ class ANNController:
         err_prev_lscale = 0.0
         t_prev = time.time()
 
-        t = 0.0
-        tick = 0
-        loss_rsum = RollingSum(100)
         while True:
-            tick += 1
             time.sleep(0.001)
             t_now = time.time()
             dt_sec = t_now - t_prev
@@ -157,35 +148,11 @@ class ANNController:
             controller._controlled_cart.set_acc((acc_applied_lscale * controller._lscale[0], 0.0))
             controller._xaction_hist_lscale.set_next(acc_applied_lscale)
 
-            controller._set_vin()
+            x = controller._get_vin()
+            y = controller._get_future_value_at_oldest_hist()
+            controller._vnet.next_data_point(x, y)
 
-            x_batch = np.array([controller._vin[0], controller._vin[0]])
-            x = tf.constant(x_batch, shape=(2, 11))
-            v_actual_past = tf.constant([controller._get_future_value_at_oldest_hist(), controller._get_future_value_at_oldest_hist()])
-            t += dt_sec
-            if t > 5.0:
-                with tf.GradientTape() as tape:
-                    tape.watch(x)
-                    v_pred_past = controller._v(x)
-                    loss = tf.losses.mean_squared_error(v_actual_past, v_pred_past)
-                gradients = tape.gradient(loss, controller._v.trainable_variables)
-                controller._optimizer.apply_gradients(zip(gradients, controller._v.trainable_variables))
-                loss_rsum.insert_next(float(tf.reduce_mean(loss)))
-
-                if tick % 100 == 0:
-                    controller._optimizer.lr = controller._base_lr * loss_rsum.mean
-                    print("Avg Loss: {:.3f}, V_pred={:.2f}, V_true={:.2f}".format(
-                        loss_rsum.mean,
-                        float(tf.reduce_mean(v_pred_past)),
-                        float(tf.reduce_mean(v_actual_past))))
-
-            if tick % 216000 == 0:
-                tf.saved_model.save(
-                    controller._v,
-                    r"D:\Users\Bazyli\Dropbox\PycharmProjects\lead-follow\model\v-{}".format(tick))
-
-            #print("V(t)={:.3f}".format(float(controller._v(controller._vin))))
-            #print("V(t)={:.3f}".format(controller.future_value_at_oldest_hist()))
+            #print("V(t) = {:.3f}".format(y))
 
 
 class _CartObsState:
@@ -202,3 +169,74 @@ class _CartObsState:
         self.pos = new_pos_scaled
         self.vel = new_vel
         self.acc = new_acc
+
+
+class _ValueNetwork:
+    def __init__(self, checkpoint_dir_path):
+        self._v = tf.keras.Sequential()
+        self._v.add(tf.keras.layers.Dense(11, activation="tanh", input_dim=11))
+        self._v.add(tf.keras.layers.Dense(7, activation="tanh"))
+        self._v.add(tf.keras.layers.Dense(3, activation="tanh"))
+        self._v.add(tf.keras.layers.Dense(1, activation=None))
+
+        self._base_lr = 0.001
+        self._lr_decay = 0.9
+        self._optimizer = tf.optimizers.SGD(self._base_lr, momentum=0.9)
+        self._loss_fn = tf.losses.mean_absolute_error
+        self._input_sz = 11
+        self._batch_sz = 16
+
+        self._batch_x = np.zeros((self._batch_sz, self._input_sz))
+        self._batch_y = np.zeros((self._batch_sz, self._input_sz))
+        self._batch_indx = 0
+
+        self._tick = 0
+        self._report_period_ticks = 100
+        self._checkpoint_period_ticks = 1000
+        self._lr_decay_period_ticks = 1000
+        self._rolling_loss = RollingSum(self._report_period_ticks)
+        self._checkpoint_dir_path = checkpoint_dir_path
+
+    def __call__(self, x):
+        return self._v(x)
+
+    def next_data_point(self, x, y):
+        self._batch_x[self._batch_indx] = x
+        self._batch_y[self._batch_indx] = y
+        self._batch_indx = (self._batch_indx + 1) % self._batch_sz
+        if self._batch_indx == 0:
+            self._backprop()
+
+    def _backprop(self):
+        self._tick += 1
+
+        x = tf.constant(self._batch_x, shape=(self._batch_sz, self._input_sz))
+        y_true = tf.constant(self._batch_y, shape=(self._batch_sz, self._input_sz))
+
+        with tf.GradientTape() as tape:
+            tape.watch(x)
+            y_pred = self(x)
+            loss = self._loss_fn(y_true, y_pred)
+
+        gradients = tape.gradient(loss, self._v.trainable_variables)
+        self._optimizer.apply_gradients(zip(gradients, self._v.trainable_variables))
+
+        self._rolling_loss.insert_next(float(tf.reduce_mean(loss)))
+
+        if self._tick % self._report_period_ticks == 0:
+            self._report()
+
+        if self._tick % self._checkpoint_period_ticks == 0:
+            self._save_checkpoint()
+
+        if self._tick % self._lr_decay_period_ticks == 0:
+            self._optimizer.lr = self._optimizer.lr * self._lr_decay
+
+    def _report(self):
+        print("Avg Loss: {:.3f}".format(
+            self._rolling_loss.mean))
+
+    def _save_checkpoint(self):
+        checkpoint_name = "v-{}-{:.5f}".format(self._tick, self._rolling_loss.mean)
+        save_path = os.path.join(self._checkpoint_dir_path, checkpoint_name)
+        tf.saved_model.save(self._v, save_path)
